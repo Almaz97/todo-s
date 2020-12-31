f2cc25b057891a2398b86c4fcaa12ec0ac3f2704
Almaz <almaz.carato@gmail.com>
2020-12-31 22:22:21 +0600

    Add caching to APIs
    Implement caching with django cache_page. There are another tools for chaching, such as redis or python memcached, but since this is a simple project, I did it with cache_page

--- todos/views.py
+++ todos/views.py
@@ -3,12 +3,20 @@ from .models import CategoryMptt, TodoList
 from rest_framework.response import Response
 from .serializers import (CategorySerializer, TodoListSerializer,
                           CategoryRetrieveSerializer)
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = CategoryMptt.objects.all()
     serializer_class = CategorySerializer
 
+    @method_decorator(cache_page(settings.CACHE_TIME))
+    def list(self, request, *args, **kwargs):
+        return super().list(self, request, args, kwargs)
+
+    @method_decorator(cache_page(settings.CACHE_TIME))
     def retrieve(self, request, *args, **kwargs):
         category = self.get_object()
         serializer = CategoryRetrieveSerializer(category)
@@ -18,3 +26,11 @@ class CategoryViewSet(ModelViewSet):
 class TodoListViewSet(ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
+
+    @method_decorator(cache_page(settings.CACHE_TIME))
+    def list(self, request, *args, **kwargs):
+        return super().list(self, request, args, kwargs)
+
+    @method_decorator(cache_page(settings.CACHE_TIME))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(self, request, args, kwargs)
